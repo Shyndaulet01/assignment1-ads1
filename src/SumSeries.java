d587b48e124f4a466ed019bcd50333c7e4278da5
Shyndaulet <254844@astanait.edu.kz>
2026-04-08 18:55:17 +0500

    SS

---
+++ src/SumSeries.java
@@ -0,0 +1,23 @@
+public class SumSeries {
+    public static double sum1(int i) {
+        if (i == 1) return 1.0; // Точка остановки
+        return (1.0 / i) + sum1(i - 1); // Шаг рекурсии
+    }
+
+    public static double sum2(int i) {
+        if (i == 1) return 1.0 / 3.0;
+        return ((double)i / (2 * i + 1)) + sum2(i - 1);
+    }
+
+    public static double sum3(int i) {
+        if (i == 1) return 1.0 / 2.0;
+        return ((double)i / (i + 1)) + sum3(i - 1);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Sum1(3): " + sum1(3));
+        System.out.println("Sum2(3): " + sum2(3));
+        System.out.println("Sum3(3): " + sum3(3));
+    }
+}
