c50d8ca6c9a67af0a206ed8261c3e4d1860ddd0f
Shyndaulet <254844@astanait.edu.kz>
2026-04-10 18:12:14 +0500

    FB!

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Fibonacci {
+    static int count = 0;
+    static int fib(int n) {
+        count++;
+        if (n <= 1) return n;
+        return fib(n-1)+fib(n-2);
+    }
+
+    public static void main(String[] args) {
+        int n=5;
+        int result = fib(n);
+        System.out.println(n+"-e чиссло Фибоначчи");
+        System.out.println("Метод был вызван раз:"+count);
+    }
 }
